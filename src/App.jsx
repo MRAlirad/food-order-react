867d9505742f07ac0897d8325923512b1f6e2085
MRAlirad <m.r.alirad75@gmail.com>
2023-11-08 21:03:55 +0330

    meal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header.jsx";
+import Meals from "./components/Meals/Meals.jsx";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   )
 }
